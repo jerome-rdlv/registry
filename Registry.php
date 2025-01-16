141bb5f5c4e52c18c7d8f54b13c87efabcf9f354
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2025-01-16 20:59:40 +0100

    Update to allow lazy service constructor callback

--- Registry.php
+++ Registry.php
@@ -5,23 +5,39 @@ namespace Rdlv\WordPress\Registry;
 class Registry
 {
     private static $services = [];
+    private static $lazies = [];
 
     /**
-     * @param object $object
-     * @param string|null $key
+     * @param  object|callable  $object  to store, or a callable factory that will be called just in time.
+     * @param  string|null  $key
+     * @param  bool  $lazy  If true, first argument will be called on first retreive and resulting object will be stored as real service
      * @return void
      */
-    public static function set(object $object, string $key = null): void
+    public static function set(object $object, string $key = null, bool $lazy = false): void
     {
+        if ($lazy) {
+            if (!is_callable($object)) {
+                throw new \Exception('In lazy mode, first argument must be callable.');
+            }
+            if ($key === null) {
+                throw new \Exception('Can not use lazy mode without a key.');
+            }
+            self::$lazies[$key] = $object;
+            return;
+        }
         self::$services[$key ?? get_class($object)] = $object;
     }
 
     /**
-     * @param string $key
+     * @param  string  $key
      * @return object|null
      */
     public static function get(string $key): ?object
     {
+        if (array_key_exists($key, self::$lazies)) {
+            self::$services[$key] = call_user_func(self::$lazies[$key]);
+            unset(self::$lazies[$key]);
+        }
         return self::$services[$key] ?? null;
     }
 }
